ac23bc79116db87a054ab0d67e62b29b8643a900
Nicolau Atala <nicolau.atala@hotmail.com>
2019-08-26 22:22:36 -0300

    Feat Upload de imagens, listando prestadores de servico

--- modulo-02/src/routes.js
+++ modulo-02/src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -12,5 +18,11 @@ routes.post('/users', UserController.store);
 // Middleware global
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+// Providers
+routes.get('/providers', ProviderController.index);
+
+// Files
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
