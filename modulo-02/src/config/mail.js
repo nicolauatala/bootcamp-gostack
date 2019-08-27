931915edce06e415d5d249eefecac53e52aaf3de
Nicolau Atala <nicolau.atala@hotmail.com>
2019-08-27 20:21:10 -0300

    Feat(modulo-02): Implementacao envio de email com nodemailer

---
+++ modulo-02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3fdb6dccb7fcab',
+    pass: 'a6a6a12cf0491e',
+  },
+  default: {
+    from: 'Equipe Gobarber',
+  },
+};
