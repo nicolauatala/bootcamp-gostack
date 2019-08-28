143f4d36e7e8eff8302d58e420486f95d72c225c
Nicolau Atala <nicolau.atala@hotmail.com>
2019-08-27 22:15:10 -0300

    Feat(modulo-02): Implementando template para envio de email com express-handlebars

--- modulo-02/src/app/controllers/AppointmentController.js
+++ modulo-02/src/app/controllers/AppointmentController.js
@@ -108,11 +108,18 @@ class AppointmentController {
 
   async delete(req, res) {
     const appointment = await Appointment.findByPk(req.params.id, {
-      include: {
-        model: User,
-        as: 'provider',
-        attributes: ['name', 'email'],
-      },
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
     });
 
     if (appointment.user_id !== req.userId) {
@@ -135,7 +142,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
